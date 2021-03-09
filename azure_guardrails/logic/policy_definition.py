85be286388c0ae896d13c71d005a81230d62fdd0
Kinnaird McQuade <kmcquade@salesforce.com>
2021-03-09 16:43:07 -0500

    Identified gap; will fix later

--- azure_guardrails/logic/policy_definition.py
+++ azure_guardrails/logic/policy_definition.py
@@ -54,6 +54,9 @@ class PolicyDefinition:
         for parameter in self.properties.parameters:
             if parameter.name == "effect":
                 continue
+            # TODO: This currently allows empty lists, which will throw an error when you try to apply it.
+            # We should allow you to print out the options to a YAML file and fill it out like a form.
+            # So right now, it will create a long Kubernetes policy, but it will have lots of empty lists that we have to fill out. Oh well.
             if not parameter.default_value and parameter.default_value != [] and parameter.default_value != "":
                 result = False
                 break
