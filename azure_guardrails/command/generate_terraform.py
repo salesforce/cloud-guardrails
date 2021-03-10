3a293e79baa66ae7ab418af99e4dbbca9bd726c4
Kinnaird McQuade <kmcquade@salesforce.com>
2021-03-10 16:54:28 -0500

    Adjust command line options

--- azure_guardrails/command/generate_terraform.py
+++ azure_guardrails/command/generate_terraform.py
@@ -31,34 +31,24 @@ supported_services_argument_values.append("all")
     callback=validate.click_validate_supported_azure_service,
 )
 @click.option(
-    "--target-name",
-    "-n",
-    "target_name",
+    "--exclude-services",
+    "exclude_services",
     type=str,
-    required=True,
-    help="The target name. Must be the name of a subscription or management group.",
-    envvar="TARGET_NAME",
-    default="example"
+    help="Exclude specific services (comma-separated) without using a config file.",
+    callback=validate.click_validate_comma_separated_excluded_services
+)
+# TODO: Mutually exclusive option groups
+# https://github.com/click-contrib/click-option-group
+# https://stackoverflow.com/questions/37310718/mutually-exclusive-option-groups-in-python-click
+@click.option(
+    "--subscription",
+    type=str,
+    help="The name of a subscription. Supply either this or --management-group",
 )
 @click.option(
-    "--target-type",
-    "-t",
-    "target_type",
-    required=True,
-    type=click.Choice(["subscription", "mg"]),
-    help="The target type - a subscription or management group.",
-    envvar="TARGET_TYPE",
-    default="subscription"
-)
-@click.option(
-    "--policy-set-name",
-    "-s",
-    "policy_set_name",
+    "--management-group",
     type=str,
-    required=True,
-    help="The name to use for the resulting Azure Policy Set/Initiative.",
-    envvar="POLICY_SET_NAME",
-    default="example"
+    help="The name of a management group. Supply either this or --subscription",
 )
 @click.option(
     "--enforce",
@@ -69,50 +59,39 @@ supported_services_argument_values.append("all")
     help="Enforce Azure Policies instead of just auditing them.",
 )
 @click.option(
-    "--module-source",
+    "--module",
     "-m",
-    "terraform_module_source",
+    "module_source",
     type=str,
     required=True,
     help="The source to use for the Terraform remote module. You can set this to your own fork or private Git Repo if you don't want to rely on this source code.",
-    envvar="TERRAFORM_MODULE_SOURCE",
+    envvar="MODULE_SOURCE",
     default=utils.DEFAULT_TERRAFORM_MODULE_SOURCE
 )
 @click.option(
-    "--config-file",
+    "--parameter-options",
+    "-o",
+    type=click.Choice(["defaults", "empty"], case_sensitive=True),
+    multiple=True,
+    required=False,
+    default=None,
+    help="Include Policies with Parameters that have default values (defaults) and/or Policies that have empty defaults that you must fill in (empty).",
+    # callback=validate.click_validate_supported_azure_service,  # TODO: Write this validation
+)
+@click.option(
+    "--config",
     "-c",
+    "config_file",
     type=click.Path(exists=False),
     required=False,
     help="The config file",
 )
 @click.option(
-    "--generate-summary",
-    "-g",
+    "--no-summary",
+    "-n",
     is_flag=True,
     default=False,
-    help="Generate a Markdown table summary showing your policies and which standards they apply to.",
-)
-@click.option(
-    "--with-parameters",
-    "-p",
-    is_flag=True,
-    default=False,
-    help="Include Policies with Parameters",
-)
-@click.option(
-    "--empty-defaults",
-    "-d",
-    "include_empty_defaults",
-    is_flag=True,
-    default=False,
-    help="Include parameters with empty defaults",
-)
-@click.option(
-    "--exclude-services",
-    "exclude_services",
-    type=str,
-    help="Exclude specific services (comma-separated) without using a config file.",
-    callback=validate.click_validate_comma_separated_excluded_services
+    help="Do not generate markdown or CSV summary files associated with the Terraform output",
 )
 @click.option(
     "-v",
@@ -120,13 +99,25 @@ supported_services_argument_values.append("all")
     "verbosity",
     count=True,
 )
-def generate_terraform(service: str, with_parameters: bool, target_name: str, target_type: str,
-                       policy_set_name: str, terraform_module_source: str, config_file: str, enforcement_mode: bool,
-                       generate_summary: bool, include_empty_defaults: bool, exclude_services: list, verbosity: int):
+def generate_terraform(
+        service: str,
+        exclude_services: list,
+        subscription: str,
+        management_group: str,
+        enforcement_mode: bool,
+        module_source: str,
+        parameter_options: list,
+        config_file: str,
+        no_summary: bool,
+        verbosity: int
+):
     """
     Get Azure Policies
     """
     set_log_level(verbosity)
+
+    # TODO: Remove initiative
+    initiative = "example"
 
     if not config_file:
         logger.info(
@@ -135,40 +126,47 @@ def generate_terraform(service: str, with_parameters: bool, target_name: str, ta
     else:
         config = get_config_from_file(config_file=config_file, exclude_services=exclude_services)
 
-    subscription_name = ""
-    management_group = ""
-    if target_type == "subscription":
-        subscription_name = target_name
+    if subscription:
+        management_group = ""
     else:
-        management_group = target_name
+        subscription = ""
 
-    if generate_summary:
-        if service == "all":
-            services = Services(config=config)
-            policy_names = services.get_display_names(with_parameters=with_parameters)
-        else:
-            services = Service(service_name=service, config=config)
-            policy_names = services.get_display_names(with_parameters=with_parameters)
-        compliance_coverage = ComplianceCoverage(display_names=policy_names)
-        markdown_table = compliance_coverage.markdown_table()
-        print(markdown_table)
+    # if generate_summary:
+    #     if service == "all":
+    #         services = Services(config=config)
+    #         policy_names = services.get_display_names(with_parameters=with_parameters)
+    #     else:
+    #         services = Service(service_name=service, config=config)
+    #         policy_names = services.get_display_names(with_parameters=with_parameters)
+    #     compliance_coverage = ComplianceCoverage(display_names=policy_names)
+    #     markdown_table = compliance_coverage.markdown_table()
+    #     print(markdown_table)
+    # else:
+    with_parameters = False
+    include_empty_defaults = False
+    parameter_options = list(parameter_options)
+    if parameter_options:
+        if "defaults" in parameter_options:
+            with_parameters = True
+        if "empty" in parameter_options:
+            include_empty_defaults = True
+
+    if service == "all":
+        services = Services(config=config)
     else:
-        if service == "all":
-            services = Services(config=config)
-        else:
-            services = Services(service_names=[service], config=config)
-        if with_parameters:
-            display_names = services.get_display_names_by_service_with_parameters(include_empty_defaults=include_empty_defaults)
-            terraform_template = TerraformTemplate(name=policy_set_name, parameters=display_names,
-                                                   subscription_name=subscription_name,
-                                                   management_group=management_group,
-                                                   enforcement_mode=enforcement_mode,
-                                                   module_source=terraform_module_source)
-            result = terraform_template.rendered()
-        else:
-            display_names = services.get_display_names_sorted_by_service(with_parameters=with_parameters)
-            result = get_terraform_template(name=policy_set_name, policy_names=display_names,
-                                            subscription_name=subscription_name,
-                                            management_group=management_group, enforcement_mode=enforcement_mode,
-                                            module_source=terraform_module_source)
-        print(result)
+        services = Services(service_names=[service], config=config)
+    if with_parameters:
+        display_names = services.get_display_names_by_service_with_parameters(include_empty_defaults=include_empty_defaults)
+        terraform_template = TerraformTemplate(name=initiative, parameters=display_names,
+                                               subscription_name=subscription,
+                                               management_group=management_group,
+                                               enforcement_mode=enforcement_mode,
+                                               module_source=module_source)
+        result = terraform_template.rendered()
+    else:
+        display_names = services.get_display_names_sorted_by_service(with_parameters=with_parameters)
+        result = get_terraform_template(name=initiative, policy_names=display_names,
+                                        subscription_name=subscription,
+                                        management_group=management_group, enforcement_mode=enforcement_mode,
+                                        module_source=module_source)
+    print(result)
