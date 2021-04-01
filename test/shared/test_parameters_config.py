659db1646d67a2cae599c49e6a21f776d157918d
Kinnaird McQuade <kmcquade@salesforce.com>
2021-04-01 12:14:02 -0400

    Parameters config template looks good

--- test/shared/test_parameters_config.py
+++ test/shared/test_parameters_config.py
@@ -88,7 +88,8 @@ class ParametersConfigTestCase(unittest.TestCase):
         policy_id = "73ef9241-5d81-4cd4-b483-8443d1730fe5"
         display_name = "API Management service should use a SKU that supports virtual networks"
         parameter_name = "effect"
-        result = self.parameters_config.get_parameter_value_from_config(display_name=display_name, parameter_name=parameter_name)
+        result = self.parameters_config.get_parameter_value_from_config(display_name=display_name,
+                                                                        parameter_name=parameter_name)
         print(result)
         # default_value is "Audit", but our user-supplied config set the value to "Deny". It should return the user-supplied value.
         self.assertEqual(result, "Deny")
@@ -97,7 +98,8 @@ class ParametersConfigTestCase(unittest.TestCase):
         policy_id = "47a1ee2f-2a2a-4576-bf2a-e0e36709c2b8"
         display_name = "Kubernetes cluster containers should not share host process ID or host IPC namespace"
         parameter_name = "namespaces"
-        results = self.parameters_config.get_parameter_value_from_config(display_name=display_name, parameter_name=parameter_name)
+        results = self.parameters_config.get_parameter_value_from_config(display_name=display_name,
+                                                                         parameter_name=parameter_name)
         print(json.dumps(results, indent=4))
         self.assertTrue(isinstance(results, list))
         self.assertListEqual(results, [])
@@ -108,7 +110,8 @@ class ParametersConfigTestCase(unittest.TestCase):
         print()
         display_name = "Only approved VM extensions should be installed"
         parameter_name = "approvedExtensions"
-        results = self.parameters_config.get_parameter_value_from_config(display_name=display_name, parameter_name=parameter_name)
+        results = self.parameters_config.get_parameter_value_from_config(display_name=display_name,
+                                                                         parameter_name=parameter_name)
         print(json.dumps(results, indent=4))
         # It should be none, because the user didn't provide anything
         self.assertIsNone(results)
@@ -119,28 +122,70 @@ class ParametersConfigTestCase(unittest.TestCase):
 
     def test_format_parameter_config_segment(self):
         class ParameterSegment:
-            def __init__(self, parameter_name: str, parameter_type: str,  value=None, default_value=None, allowed_values: list = None):
+            def __init__(self, parameter_name: str, parameter_type: str, value=None, default_value=None,
+                         allowed_values: list = None):
                 self.name = parameter_name
                 self.type = parameter_type
                 self.allowed_values = allowed_values
                 self.default_value = default_value
                 self.value = value
+
         allowed_values_metric_name = None
         metric_name = ParameterSegment(parameter_name="metricName", parameter_type="String", default_value=None,
                                        value=None, allowed_values=allowed_values_metric_name)
         allowed_values_effect = ["AuditIfNotExists", "Disabled"]
         effect = ParameterSegment(parameter_name="effect", parameter_type="String", default_value="Audit",
                                   value="Audit", allowed_values=allowed_values_effect)
-        parameter_segments = [metric_name, effect]
+
+        deny_list_of_stuff = ["Value1", "Value2"]
+        list_of_stuff = ParameterSegment(parameter_name="denyListOfStuff", parameter_type="String",
+                                         default_value="Audit",
+                                         value=deny_list_of_stuff, allowed_values=deny_list_of_stuff)
+        empty_list_param = ParameterSegment(parameter_name="emptyListParam", parameter_type="String",
+                                            default_value=None,
+                                            value=[], allowed_values=["something", "something", "dark", "side"])
+
+        categorized_parameters = {
+            "Batch": {
+                "Metric alert rules should be configured on Batch accounts": [
+                    metric_name,
+                    effect,
+                    list_of_stuff,
+                    empty_list_param
+                ]
+            }
+        }
+
         template_contents = dict(
-            parameter_segments=parameter_segments
+            # parameter_segments=parameter_segments,
+            categorized_parameters=categorized_parameters
         )
         template_path = os.path.join(os.path.dirname(__file__))
         env = Environment(loader=FileSystemLoader(template_path), lstrip_blocks=True)  # nosec
         env.tests['is_none_instance'] = utils.is_none_instance
-        template = env.get_template("parameter-yaml-segment.yml")
+
+        def is_list(value):
+            return isinstance(value, list)
+
+        env.tests['is_a_list'] = is_list
+
+        template = env.get_template("parameter-yaml-segment.yml.j2")
         result = template.render(t=template_contents)
         print(result)
+        expected_result = """# ---------------------------------------------------------------------------------------------------------------------
+# Batch
+# ---------------------------------------------------------------------------------------------------------------------
+Batch:
+  Metric alert rules should be configured on Batch accounts:
+    metricName: # Note: No default parameters
+    effect: Audit  # Allowed: ["AuditIfNotExists", "Disabled"]
+    denyListOfStuff:
+        - Value1
+        - Value2 # Allowed: ["Value1", "Value2"]
+    emptyListParam: [] # Note: No default parameters # Allowed: ["something", "something", "dark", "side"]
+"""
+        self.assertEqual(result, expected_result)
+        # print()
 
 
 class TerraformParameterV4TestCase(unittest.TestCase):
